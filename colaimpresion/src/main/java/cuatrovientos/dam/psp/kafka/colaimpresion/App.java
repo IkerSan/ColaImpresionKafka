ae6ab6458b814b6414d5c80cf4df494002f5c74d
IkerSan <cosasquequieres@gmail.com>
2026-01-29 10:06:50 +0100

    Estructura base del proyecto

---
+++ colaimpresion/src/main/java/cuatrovientos/dam/psp/kafka/colaimpresion/App.java
@@ -0,0 +1,13 @@
+package cuatrovientos.dam.psp.kafka.colaimpresion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
